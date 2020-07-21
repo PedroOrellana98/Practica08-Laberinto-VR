aba33f2cb7b64ab7250ee07b1025d97b24b4aeb2
PedroOrellana98 <porellanaj@est.ups.edu.ec>
2020-07-21 15:07:39 -0500

    Commit de finalizacion
    Commit de finalizacion del proyecto

---
+++ Temp/gradleOut/build/generated/source/buildConfig/release/com/ec/edu/ups/laberinto/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ec.edu.ups.laberinto;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.ec.edu.ups.laberinto";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
